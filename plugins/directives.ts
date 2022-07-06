c30095b2a4e35ed8179771020daa2157aaef6e88
Johann Schopplich <mail@johannschopplich.com>
2022-07-06 19:30:25 +0200

    fix: hoisting html if no nodes included

--- plugins/directives.ts
+++ plugins/directives.ts
@@ -4,11 +4,14 @@ export default defineNuxtPlugin((nuxtApp) => {
   const { vueApp } = nuxtApp
 
   /**
-   * Directive to replace the current element with its raw HTML contents
-   * Especially useful with ` <div v-hoist v-html="html" />` to replace the
-   * wrapping `<div>` element with raw HTML
+   * Directive to replace the current element with its raw HTML content
+   * Especially useful with `<div v-hoist v-html="html" />` to replace the
+   * wrapping `<div>` element with its raw inner HTML
    */
   vueApp.directive('hoist', (el) => {
+    // Return if no child elements of type element nodes are found
+    if (![...el.childNodes].some((i) => i.nodeType === 1)) return
+
     if (el.tagName === 'TEMPLATE') {
       el.replaceWith(el.content)
     } else {
