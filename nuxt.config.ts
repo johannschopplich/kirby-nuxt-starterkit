8e3cce52a9636b68f4a667f83ce85b0413c2ae12
Johann Schopplich <mail@johannschopplich.com>
2022-08-01 21:53:26 +0200

    chore: workaround for Netlify bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,16 @@ export default defineNuxtConfig({
     auth: 'bearer',
   },
 
+  hooks: {
+    'vite:extendConfig'(config, { isServer }) {
+      if (isServer) {
+        // Workaround for Netlify issue
+        // https://github.com/nuxt/framework/issues/6204
+        config.build.rollupOptions.output.inlineDynamicImports = true
+      }
+    },
+  },
+
   typescript: {
     shim: false,
   },
