845ecc80ce5c337a6be82acad94d2e87dab5daeb
Johann Schopplich <mail@johannschopplich.com>
2022-08-15 08:04:41 +0200

    chore: update ts error comment

--- components/Kirby/Blocks.vue
+++ components/Kirby/Blocks.vue
@@ -6,7 +6,7 @@ import {
   KirbyBlockList,
   KirbyBlockQuote,
   KirbyBlockText,
-  // @ts-expect-error: Will be fixed in v3.0.0-rc.9
+  // @ts-expect-error: To be fixed in Nuxt upstream
 } from '#components'
 import type { ComponentPublicInstance } from 'vue'
 import type { KirbyBlock } from '#nuxt-kql'
