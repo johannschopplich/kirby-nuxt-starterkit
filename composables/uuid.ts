136bbf7827de538a9470c8c16263b28d9f06921d
Johann Schopplich <mail@johannschopplich.com>
2023-06-14 10:53:58 +0200

    refactor: remove `createUuidResolver`

--- composables/uuid.ts
+++ composables/uuid.ts
@@ -7,12 +7,3 @@ export function resolveUuid<T extends Record<string, any>>(
 ) {
   return collection.find((i) => i.uuid === uuid)
 }
-
-/**
- * Returns a function that resolves a UUID to an entity in a collection
- */
-export function createUuidResolver<T extends Record<string, any>>(
-  collection: T[]
-) {
-  return (uuid: string) => resolveUuid(collection, uuid)
-}
