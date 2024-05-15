7464bc1e9d30038f4cf5ea37aa8763b7fe1b87df
Johann Schopplich <mail@johannschopplich.com>
2024-05-15 11:12:54 +0200

    chore: simplify ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,9 @@ import antfu from '@antfu/eslint-config'
 export default await antfu(
   {
     stylistic: false,
-    ignores: ['tsconfig.json'],
   },
   {
     rules: {
-      'node/prefer-global/buffer': 'off',
-      'node/prefer-global/process': 'off',
       // Ignore rules clashing with Prettier
       'vue/html-closing-bracket-newline': 'off',
       'vue/html-indent': 'off',
